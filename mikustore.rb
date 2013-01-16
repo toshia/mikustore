fcea1b32f9b3cf62889a438e82d8835a4d787aaa
Toshiaki Asai <qtoship@gmail.com>
2013-01-17 01:41:45 +0900

    非公式リポジトリに対応した

--- mikustore.rb
+++ mikustore.rb
@@ -8,5 +8,11 @@ Plugin.create(:mikustore) do
     add Plugin::Mikustore::StoreFrame.new
   end
 
+  filter_mikustore_plugins do |plugins|
+    Dir[File.expand_path(File.join(File.dirname(__FILE__), "packages", "*"))].each{ |package_file|
+      plugins << YAML.load_file(package_file).symbolize
+    }
+    [plugins]
+  end
 end
 
---
+++ packages/toshi_a_store
@@ -0,0 +1,11 @@
+---
+slug: :toshi_a_store
+depends:
+  mikutter: 0.2.1.1119
+  plugin:
+    - mikustore
+version: '1.0'
+author: toshi_a
+name: toshi_a's store
+description: toshi_aの書いたmikutterプラグインをみっくストアに追加するプラグイン
+repository: git://github.com/toshia/toshi_a_store.git
